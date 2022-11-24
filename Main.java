80fe48be00712e718d1097968934e696c1494aab
Flakiator <niklaschristensen2002@gmail.com>
2022-11-24 12:32:57 +0100

    Made methods for constructing UI to clean up main method

--- Main.java
+++ Main.java
@@ -3,23 +3,32 @@ import javax.swing.*;
 
 public class Main
 {
+    private static JFrame frame;
     public static void main(String[] args)
     {
-        JFrame frame = new JFrame(); //Laver en frame
-        frame.setVisible(true); //frame bliver synlig
-        frame.setTitle("Netflæsk"); //Navnet på applikation
+        makeFrame();
+    }
+    private static void makeFrame()
+    {
+        frame = new JFrame("Netflæsk"); //Laver en frame
         //Gør så vores program rent faktisk lukker.
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Den har en standard size, ikke redigerbar af client
         frame.setResizable(false);
-
         //frame.getContentPane().setBackground(Color.white); //Kan også laves med 'new Color'.
 
+        makeMenuBar();
+        frame.setSize(800,800); //x og y
+        frame.setVisible(true); //frame bliver synlig
+    }
+
+    private static void makeMenuBar()
+    {
         //Laver MenuBar
         JMenuBar menuBar = new JMenuBar();
         frame.setJMenuBar(menuBar);
         JMenu myList = new JMenu("My List");
         menuBar.add(myList);
-        frame.setSize(800,800); //x og y
     }
+
 }
