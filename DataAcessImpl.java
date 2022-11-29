709219dd47e671259fb1b71f3fcac91a21ff1945
Flakiator <niklaschristensen2002@gmail.com>
2022-11-29 18:00:31 +0100

    Tilføjet kommentarer til DataAcessImpl

--- DataAcessImpl.java
+++ DataAcessImpl.java
@@ -13,17 +13,25 @@ public class DataAcessImpl implements DataAcess
 
     public List<String> loadMedia(String path)
     {
+        // Laver en liste medie navnene kan stå i
         List<String> media = new ArrayList<>();
         try {
+            // Giver file navnet på vejen til filen
             File file = new File(path);
+            // Laver et scanner objekt
             Scanner myReader = new Scanner(file);
+            // Laver et while loop der bliver ved indtil der ikke er flere linjer i txt filen
             while (myReader.hasNextLine())
             {
+                // Sætter en linje ind i strengen "data"
                 String data = myReader.nextLine();
+                // Tilføjer data som element i "media"
                 media.add(data);
             }
+            // Lukker myReader da vi nu har læst hele txt filen igennem
             myReader.close();
             return media;
+            // tager højde for hvis filen ikke eksisterer
         } catch (FileNotFoundException e)
         {
             System.out.println("An error occurred.");
@@ -45,9 +53,13 @@ public class DataAcessImpl implements DataAcess
 
     public List<String> loadPic(String path)
     {
+        // Laver en liste billede navnene kan stå i
         List<String> mediaPic = new ArrayList<>();
+        // Giver file navnet på vejen til filen
         File file = new File(path);
+        // bruger gemmer alle billede navnene i en liste "allFiles" med listFiles metoden
         File[] allFiles = file.listFiles();
+        // itererer gennem allFiles og gemmer navnene på billederne i "mediaPic"
         for (int i = 0; i < allFiles.length; i++)
         {
             mediaPic.add(String.valueOf(allFiles[i]));
