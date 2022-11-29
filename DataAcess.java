3d11896b88e0ef93735e764f19824d7a80bed589
Flakiator <niklaschristensen2002@gmail.com>
2022-11-29 14:33:09 +0100

    Lavet data interface og klasse

---
+++ DataAcess.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public interface DataAcess
+{
+    List<String> loadMovies();
+    List<String> loadSeries();
+    List<String> loadMPic();
+    List<String> loadSPic();
+    List<String> loadFavorites();
+    void saveFavorites(List<String> favorites);
+}
---
+++ DataAcessImpl.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class DataAcessImpl implements DataAcess{
+    public List<String> loadMovies()
+    {
+        return null;
+    }
+    public List<String> loadSeries()
+    {
+        return null;
+    }
+    public List<String> loadMPic()
+    {
+        return null;
+    }
+    public List<String> loadSPic()
+    {
+        return null;
+    }
+    public List<String> loadFavorites()
+    {
+        return null;
+    }
+    public void saveFavorites(List<String> favorites)
+    {
+
+    }
+}
