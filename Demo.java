39ebf90862ddf8a95c7c0b22c1114bc3fdc743c6
Flakiator <niklaschristensen2002@gmail.com>
2022-12-05 11:50:29 +0100

    Korte test for mediareg i demo

--- Demo.java
+++ Demo.java
@@ -1,14 +1,39 @@
 import java.util.*;
+
 public class Demo
 {
     public static void main(String[] args)
     {
-        DataAcessImpl dinmor = new DataAcessImpl();
-        List<String>  movies = dinmor.loadMovies();
+            // Laver et DataAcess objekt
+            DataAcessImpl data = new DataAcessImpl();
+            // Sætter de nødvændige lister op
+            List<String> movie = data.loadMovies();
+            List<String> movieP = data.loadMPic();
+            List<String> series = data.loadSeries();
+            List<String> seriesP = data.loadSPic();
 
-        for (String lort : movies)
-        {
-            System.out.println(lort);
+            for (int i = 0; i < movie.size();i++)
+            {
+                // Opdeler alle film (og deres, årstal genre osv. i en liste "elements")
+                String[] elements = movie.get(i).split(";");
+                // Test print
+                //System.out.println(elements[0].trim());
+                // Laver opdeler genrer i sin egen liste
+                String[] genres = elements[2].split(",");
+                // tilføjer dem til ArrayList
+                List<String> genre = new ArrayList<>();
+                for (int j = 0; j < genres.length; j++)
+                {
+                    genre.add(genres[j].trim());
+                }
+                System.out.println("genre til: " + elements[0]);
+                for (String g: genre)
+                {
+                    System.out.println(g);
+                }
+
+                //for ()
+                //new Media(elements[0],movieP.get(i),);
+            }
         }
     }
-}
--- Mediaregimpl.java
+++ Mediaregimpl.java
@@ -1,5 +1,24 @@
-
+import java.util.*;
 public class Mediaregimpl
 {
+    public List<Media> initialize()
+    {
+        // Laver et DataAcess objekt
+        DataAcessImpl data = new DataAcessImpl();
+        // Sætter de nødvændige lister op
+        List<String> movie = data.loadMovies();
+        List<String> movieP = data.loadMPic();
+        List<String> series = data.loadSeries();
+        List<String> seriesP = data.loadSPic();
 
+        for (int i = 0; i < movie.size(); i++);
+        {
+            String[] elements = movie.get(0).split(";");
+            for (String e: elements)
+            {
+                System.out.println(e);
+            }
+        }
+        return null;
+    }
 }
