f1a509566c0a445320dc92476e6b48d757124f5d
Flakiator <niklaschristensen2002@gmail.com>
2022-12-13 14:01:03 +0100

    Opdateret Mediaregistry interface

--- Domain/Mediaregistry.java
+++ Domain/Mediaregistry.java
@@ -6,4 +6,7 @@ public interface Mediaregistry
     public List<MediaImpl> initializeAllMedia();
     public List<MediaImpl> search(String SearchText, List<MediaImpl>FilteredList);
     public List<MediaImpl> filter(String Genre, String Medietype, List<MediaImpl>AllMedia);
+    public String[] getAllGenre(List<MediaImpl> medias);
+    public void addToFavorites(MediaImpl media);
+    public void removeFromFavorites(MediaImpl media);
 }
