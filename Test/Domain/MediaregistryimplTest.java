cb79e13c3f213fdde4c87be627f3656833e04fbd
Flakiator <niklaschristensen2002@gmail.com>
2022-12-14 22:22:08 +0100

    Gjort så vi kan teste vores metoder

---
+++ Test/Domain/MediaregistryimplTest.java
@@ -0,0 +1,32 @@
+package Domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MediaregistryimplTest {
+
+    @Test
+    public void initializeAllMedia() {
+    }
+
+    @Test
+    public void search() {
+    }
+
+    @Test
+    public void filter() {
+    }
+
+    @Test
+    public void getAllGenre() {
+    }
+
+    @Test
+    public void addToFavorites() {
+    }
+
+    @Test
+    public void removeFromFavorites() {
+    }
+}
