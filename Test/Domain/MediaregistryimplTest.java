6f3d5477648576c66a5f137f1ac12e7f24e44ffd
Flakiator <niklaschristensen2002@gmail.com>
2022-12-15 21:29:39 +0100

    Lavet flere unit test

--- Test/Domain/MediaregistryimplTest.java
+++ Test/Domain/MediaregistryimplTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,7 +31,7 @@ class MediaregistryimplTest {
     }
 
     @Test
-    void initializeAllMedia() {
+    void initializeAllMediaammounts() {
         // sætter medias, movies og series til forskellige værdier
         int medias = 0;
         int movies = 1;
@@ -44,21 +46,59 @@ class MediaregistryimplTest {
         // ser om der er lige mange medier læst i data basen som der bliver lavet om til objekter
         assertEquals(medias,series + movies);
     }
+    @Test
+    void initializeAllMediaduplicates() {
+        // sætter medias, movies og series til forskellige værdier
+        List<MediaImpl> medias = new ArrayList<>();
+        int count = 0;
+        try {
+            medias = mediaReg.initializeAllMedia();
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        List<String> titles = new ArrayList<>();
+        for (MediaImpl m : medias)
+        {
+            titles.add(m.getTitle());
+        }
+        Set<String> unique_titles = new HashSet<>(titles);
+        // Plus 1 fordi der både er en serie og film der hedder "fargo"
+        if (unique_titles.size() + 1 < titles.size())
+        {
+            count++;
+        }
+        // Ser om der bliver lavet flere af de samme film
+        assertEquals(0,count);
+    }
 
     @Test
-    void search() {
-        String searchtext = "Breaking bad";
+    void search()
+    {
+        String searchtext = "Bad";
         List<MediaImpl> medias;
+        List<MediaImpl> themedias;
+        int count = 0;
         try {
             medias = mediaReg.initializeAllMedia();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        assertEquals(1,mediaReg.search(searchtext,medias).size());
+        themedias = mediaReg.search(searchtext,medias);
+        for (MediaImpl m : themedias)
+        {
+            if (m.getTitle().contains("Bad"))
+            {
+                System.out.println(m.getTitle());
+                count++;
+            }
+        }
+        // Sammenligner antallet af film med Bad i navnet med antallet af film med Bad i navnet som search returnerer
+        assertEquals(2,themedias.size());
     }
 
     @Test
     void filter() {
+
     }
 
     @Test
